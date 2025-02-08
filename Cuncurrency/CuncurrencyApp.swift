092dd2d1bd504483ee31f5f761b83d7cd027a23b
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-02-08 14:21:54 +0330

    Initial Commit

---
+++ Cuncurrency/CuncurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuncurrencyApp.swift
+//  Cuncurrency
+//
+//  Created by Sothesom on 20/11/1403.
+//
+
+import SwiftUI
+
+@main
+struct CuncurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
